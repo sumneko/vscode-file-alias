443e5b09580ca31397084437b4222a8f3ce5be00
最萌小汐 <sumneko@hotmail.com>
2021-10-12 18:20:41 +0800

    cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,21 @@
-import { workspace, ExtensionContext } from 'vscode';
+import { FileDecoration, Uri, window } from 'vscode';
 
-export function activate(context: ExtensionContext) {
+// hack VSCode
+// @ts-ignore
+FileDecoration.validate = () => {
+    return true;
+}
+
+class FileAlias {
+    async provideFileDecoration(uri: Uri): Promise<FileDecoration> {
+        let fd = new FileDecoration(uri.toString())
+        return fd
+    }
+}
+
+export function activate() {
+    let fileAlias = new FileAlias();
+    window.registerFileDecorationProvider({
+        provideFileDecoration: fileAlias.provideFileDecoration,
+    })
 }
